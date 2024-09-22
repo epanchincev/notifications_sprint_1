dfbb109b8670bdffc9a3952672f8d3531a5057cf
DBWtv <dima.bwc@gmail.com>
2024-09-22 11:08:55 +0500

    Поменял название на правильное

--- notifications_api/src/api/v1/register_notification/handlers.py
+++ notifications_api/src/api/v1/register_notification/handlers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from src.services.consumer import IProducerService, get_queue_producer
+from notifications_api.src.services.producer import IProducerService, get_queue_producer
 
 router = APIRouter(
     prefix="/register-notifications",
