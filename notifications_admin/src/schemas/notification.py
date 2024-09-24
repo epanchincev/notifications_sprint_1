402e92b3417c9b27c4ac6f178c661a19a5bd3b07
epanchincev <alexei.epanchincev@yandex.ru>
2024-09-24 13:38:38 +0500

    added model validators

--- notifications_admin/src/schemas/notification.py
+++ notifications_admin/src/schemas/notification.py
@@ -1,6 +1,6 @@
 from enum import StrEnum
 
-from pydantic import BaseModel, ConfigDict, UUID4
+from pydantic import BaseModel, ConfigDict, UUID4, model_validator, Field
 
 
 class NotificationType(StrEnum):
@@ -18,7 +18,7 @@ class NotificationStatus(StrEnum):
 class NotificationBase(BaseModel):
 
     template_id: UUID4
-    recepients: list[UUID4]
+    recepients: list[UUID4] = Field(min_length=1)
     schedule: str | None = None
 
 
@@ -28,6 +28,27 @@ class NotificationCreate(NotificationBase):
     name: str | None = None
     type: NotificationType = NotificationType.insta
     delay_in_minutes: int | None = None
+
+    @model_validator(mode="after")
+    def check_delay(self):
+        if (
+            self.type == NotificationType.delayed
+            and self.delay_in_minutes is None
+        ):
+            raise ValueError(
+                "delay_in_minutes is required for delayed notifications",
+            )
+
+        return self
+
+    @model_validator(mode="after")
+    def check_schedule(self):
+        if self.type == NotificationType.scheduled and self.schedule is None:
+            raise ValueError(
+                "schedule is required for scheduled notifications"
+            )
+
+        return self
 
 
 class NotificationDB(NotificationBase):
