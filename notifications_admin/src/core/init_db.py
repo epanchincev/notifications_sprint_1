d282aa6b6b5506e70f82fe8c4532353deef6b35b
epanchincev <alexei.epanchincev@yandex.ru>
2024-09-26 15:20:24 +0500

    create register template on start app

--- notifications_admin/src/core/init_db.py
+++ notifications_admin/src/core/init_db.py
@@ -1,18 +1,25 @@
 from contextlib import asynccontextmanager
 
 from db.postgres import get_async_session
+from repositories.template import ITemplateRepository, get_template_repository
 
 
 get_async_session_context = asynccontextmanager(get_async_session)
 
 
-async def create_basic_templates(templates_rep) -> None:
-    pass
+async def create_basic_templates(templates_rep: ITemplateRepository) -> None:
+    template = await templates_rep.get_by_name('register')
+    if not template:
+        await templates_rep.create(
+            'register',
+            'email',
+            'Регистрация в сервисе',
+            'Поздравляем %username%, Вы успешно зарегистрировались в сервисе!',
+            ['username'],
+        )
 
 
 async def init_db():
-    """
-    Корутина инициализирует базу данных.
-
-    """
-    pass
+    async with get_async_session_context() as session:
+        template_rep: ITemplateRepository = get_template_repository(session)
+        await create_basic_templates(template_rep)
--- notifications_admin/src/main.py
+++ notifications_admin/src/main.py
@@ -10,11 +10,13 @@ from fastapi import (
 from api.router import router as api_router
 from services.exceptions.base import BaseServiceException
 from core.config import settings
+from core.init_db import init_db
 from utils import send_notification
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    await init_db()
     send_notification.session = ClientSession()
     yield
     await send_notification.session.close()
